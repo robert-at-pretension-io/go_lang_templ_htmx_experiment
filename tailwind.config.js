baa0681f8c2b903541e72e1f9329baeba0f29404
elliot <robert@pretension.io>
2024-03-13 18:32:04 -0400

    Added the custom two fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "fraunces": ['Fraunces', 'serif']
+        "fraunces": ['Fraunces', 'serif'],
+        "playfair": ['Playfair Display', 'serif'],
+        "prata": ['Prata', 'serif']
       }
     },
   },
