011200ce17065b7ce8f0bcb18e1e95f10389fb55
elliot <robert@pretension.io>
2024-03-15 15:41:55 -0400

    Added the loading placeholders

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
         "fraunces": ['Fraunces', 'serif'],
         "playfair": ['Playfair Display', 'serif'],
         "prata": ['Prata', 'serif']
-      }
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'translateX(10%)' },
+          '50%': { transform: 'translateX(80%)' },
+        },
+      },
+      animation: {
+        'back-and-forth': 'wiggle 4s ease-in-out infinite',
+      },
     },
   },
   plugins: [require('@tailwindcss/typography'),],
