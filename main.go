b84abed960c2a5b041f32afc9171e46016a700e3
elliot <robert@pretension.io>
2024-03-16 15:10:55 -0400

    template is working, woo lad

--- main.go
+++ main.go
@@ -1,43 +1,67 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"net/http"
+	"os"
+
+	"time"
 )
 
-func getUsername(w http.ResponseWriter, r *http.Request) {
-    // Ensure we're dealing with a POST request
-    if r.Method != "POST" {
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-        return
-    }
+func getUsername(w http.ResponseWriter, r *http.Request) (string, error) {
+	if r.Method != "POST" {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return "", nil
+	}
 
-    // Parse form data
-    if err := r.ParseForm(); err != nil {
-        return
-    }
+	if err := r.ParseForm(); err != nil {
+		return "", err
+	}
 
-    // Get the username from the form
-    username := r.FormValue("username")
-
+	username := r.FormValue("username")
 	return username, nil
 }
 
+func handleDelayedReturn(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "<div>return</div>")
+}
 
 func main() {
-	http.HandleFunc("/submit_username", func(w http.ResponseWriter, r *http.Request) {
+	fs := http.FileServer(http.Dir("."))
 
-		username, err := getUsername(w, r)
-		if err != nil {
-			component := hello(username)
-			component.Render(context.Background(), w) // Use w instead of os.Stdout
-		}
-		else {
-
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		// Check if the request is for the root or for index.html, and not a request for a static file
+		if path == "/" || path == "/index.html" {
+			component := chat_page()
+			component.Render(r.Context(), w)
+		} else {
+			// Check if the file exists in the current directory
+			if _, err := os.Stat("." + path); err == nil || !os.IsNotExist(err) {
+				// File exists, let the FileServer handler serve it
+				fs.ServeHTTP(w, r)
+			} else {
+				// File doesn't exist, return a 404
+				http.NotFound(w, r)
+			}
 		}
 	})
 
+	http.HandleFunc("/submit_username", func(w http.ResponseWriter, r *http.Request) {
+		username, err := getUsername(w, r)
+		if err == nil {
+			// Placeholder for where you'd use the username and render a response
+			fmt.Fprintf(w, "<div>Hello, %s</div>", username)
+		}
+	})
 
+	http.HandleFunc("/delayed_return", handleDelayedReturn)
 
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
