afcf2981bc26d34f7d104a635ed056b78fc1ab44
elliot <robert@pretension.io>
2024-03-23 11:43:54 -0400

    The login page is almost fully incorporated, just need to get the page wrapping completed

--- hello_templ.go
+++ hello_templ.go
@@ -244,7 +244,15 @@ func chat_page() templ.Component {
 			templ_7745c5c3_Var10 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Local Tailwind CSS Example</title><link rel=\"stylesheet\" href=\"./dist/output.css\"><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght,SOFT,WONK@0,9..144,100..900,0..100,0..1;1,9..144,100..900,0..100,0..1&amp;family=Playfair+Display:ital,wght@0,400..900;1,400..900&amp;family=Prata&amp;family=Roboto:wght@100;900&amp;display=swap\" rel=\"stylesheet\"><script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script></head><body><div><div id=\"chat\" class=\"grid grid-cols-10 shadow-md to-orange-300 bg-gradient-to-t from-red-200 text-black w-full\"></div></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = header().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<body><div><div id=\"chat\" class=\"grid grid-cols-10 shadow-md to-orange-300 bg-gradient-to-t from-red-200 text-black w-full\"></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -253,6 +261,86 @@ func chat_page() templ.Component {
 			return templ_7745c5c3_Err
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</body></html>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func reload_page() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var11 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var11 == nil {
+			templ_7745c5c3_Var11 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<script>\n    document.body.addEventListener('htmx:afterOnLoad', function(event) {\n        // Reload the page\n        window.location.reload();\n    });\n</script>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func header() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var12 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var12 == nil {
+			templ_7745c5c3_Var12 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Local Tailwind CSS Example</title><link rel=\"stylesheet\" href=\"./dist/output.css\"><link rel=\"preconnect\" href=\"https://fonts.googleapis.com\"><link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin><link href=\"https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght,SOFT,WONK@0,9..144,100..900,0..100,0..1;1,9..144,100..900,0..100,0..1&amp;family=Playfair+Display:ital,wght@0,400..900;1,400..900&amp;family=Prata&amp;family=Roboto:wght@100;900&amp;display=swap\" rel=\"stylesheet\"><script src=\"https://unpkg.com/htmx.org@1.9.10\" integrity=\"sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC\" crossorigin=\"anonymous\"></script></head>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		if !templ_7745c5c3_IsBuffer {
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteTo(templ_7745c5c3_W)
+		}
+		return templ_7745c5c3_Err
+	})
+}
+
+func login() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
+		if !templ_7745c5c3_IsBuffer {
+			templ_7745c5c3_Buffer = templ.GetBuffer()
+			defer templ.ReleaseBuffer(templ_7745c5c3_Buffer)
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var13 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var13 == nil {
+			templ_7745c5c3_Var13 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<!doctype html><html lang=\"en\">")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = header().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<body><div id=\"login_container\"><div id=\"login\" class=\"grid grid-cols-8 grid-rows-3 to-orange-300 bg-gradient-to-t from-red-200 text-black w-full h-[100vh]\"><div class=\"col-span-5 row-span-1 row-start-2 col-start-2\n                    \n                    md:col-start-3 md:col-span-4\n                    m-4  p-4 text-center font-fraunces prose min-w-1.5 border-l-4 md:border-t-4 md:border-l-0 border-white w-full \"><h1>Login</h1><form class=\"grid-rows-2 grid-cols-1 col-span-3 \"><input type=\"text\" id=\"phone\" name=\"phone_number\" placeholder=\"cellphone\" class=\"active:outline-none text-center\n                            flex-col w-full tracking-widest \"> <button hx-post=\"/login\" hx-target=\"#login_container\" hx-swap=\"outerHTML\" class=\"my-2  px-4 bg-white text-center w-full lowercase prose font-extrabold\"><b>Submit</b></button></form></div></div></div></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- main.go
+++ main.go
@@ -29,6 +29,18 @@ func handleDelayedReturn(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<div>return</div>")
 }
 
+func get_form_value(r *http.Request, w http.ResponseWriter, form_value string) (string, string) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return "", "failed to parse string"
+	}
+
+	message := r.FormValue(form_value)
+	fmt.Printf("Received message: %s\n", message)
+
+	return message, ""
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("."))
 
@@ -36,8 +48,30 @@ func main() {
 		path := r.URL.Path
 		// Check if the request is for the root or for index.html, and not a request for a static file
 		if path == "/" || path == "/index.html" {
-			component := chat_page()
-			component.Render(r.Context(), w)
+			// Attempt to retrieve the cookie by name
+			cookie, err := r.Cookie("phone_number")
+			// Check if the cookie was found
+			if err != nil {
+				if err == http.ErrNoCookie {
+					// Cookie not found, handle accordingly
+					fmt.Printf("Cookie is not set.")
+
+					component := login()
+					component.Render(r.Context(), w)
+					// Optionally, you can set the cookie here if it's necessary
+				} else {
+					// Some other error occurred
+					http.Error(w, "An error occurred while checking the cookie", http.StatusInternalServerError)
+				}
+			} else {
+				// Cookie is found, handle accordingly
+				fmt.Printf("Cookie is set: %s", cookie.Value)
+				component := chat_page()
+				component.Render(r.Context(), w)
+			}
+
+			// Check to see if there is a cookie set named phone number:
+
 		} else {
 			// Check if the file exists in the current directory
 			if _, err := os.Stat("." + path); err == nil || !os.IsNotExist(err) {
@@ -61,15 +95,26 @@ func main() {
 	http.HandleFunc("/delayed_return", handleDelayedReturn)
 
 	http.HandleFunc("/submit_chat", func(w http.ResponseWriter, r *http.Request) {
-		if err := r.ParseForm(); err != nil {
-			http.Error(w, "Failed to parse form", http.StatusBadRequest)
-			return
-		}
-
-		message := r.FormValue("user_prompt")
-		fmt.Printf("Received message: %s\n", message)
+		message, _ := get_form_value(r, w, "user_prompt")
 
 		component := user_input_submitted(message)
+		component.Render(r.Context(), w)
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		phone_number, _ := get_form_value(r, w, "phone_number")
+
+		cookie := &http.Cookie{
+			Name:     "phone_number",                 // Cookie name
+			Value:    phone_number,                   // Cookie value
+			Expires:  time.Now().Add(24 * time.Hour), // Set the expiration time for 1 day
+			Path:     "/",                            // Path for the cookie
+			HttpOnly: true,                           // Makes the cookie inaccessible to JavaScript running in the browser
+		}
+
+		http.SetCookie(w, cookie)
+
+		component := reload_page()
 		component.Render(r.Context(), w)
 	})
 
@@ -77,4 +122,5 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Failed to start server:", err)
 	}
+
 }
